4b194e0e59b5583d6f5aa0247c099c31f54aea36
hannahssstone <113951483+hannahssstone@users.noreply.github.com>
2022-10-20 02:17:25 -0500

    Update embedded.cpp

--- embeddedPico/embedded.cpp
+++ embeddedPico/embedded.cpp
@@ -1,12 +1,21 @@
 //this is the place for the code on the pico//
 #include <Arduino.h>
+#include <Wire.h>
 
- void setup()
- {
-   
- }
+#define I2C_SDAPIN = 6
+#define I2C_SCLPIN = 7
+#define UART_TX_PIN = 1
+#define UART_RX_PIN = 2
 
- void loop()
- {
-   
- }
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.setRX(UART_RX_PIN);
+  Serial1.setTX(UART_TX_PIN);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
